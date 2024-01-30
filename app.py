aeccfa2359b9d0c5cf018df33064f2db17499348
Diganta Biswas <51294318+Riponz@users.noreply.github.com>
2024-01-31 00:02:03 +0530

    initial comit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from joblib import load
+import pandas as pd
+
+scalar = load("scalar.joblib")
+model = load("chronicKidney.joblib")
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+@app.route('/chronic-kidney', methods=['POST'])
+def kidney():
+    try:
+        data = request.get_json()
+        data_val=[list(data.values())]
+
+        dataframe = pd.DataFrame(data_val)
+        print(dataframe)
+        dataframe.iloc[:, [0,2,4,6,7]] = scalar.transform(dataframe.iloc[:, [0,2,4,6,7]])
+
+        predict = model.predict(dataframe)[0]
+        print(predict)
+
+        return jsonify({'class' : str(predict)})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
